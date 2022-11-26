0c5494c6bb0c7098d455f92e1049f14164b59aa2
Campbell <campbell.wass@gmail.com>
2022-11-26 22:30:46 +1300

    Switch order of loop counters in matix multiplication. This reduces cache misses and GREATLY increasing vectorization, improving perf by about 50%. Aslo add tests for multiplication

--- crates/network/src/util.rs
+++ crates/network/src/util.rs
@@ -1,4 +1,9 @@
-use std::{fmt, marker::PhantomData};
+use std::{
+    fmt,
+    iter::Sum,
+    marker::PhantomData,
+    ops::{AddAssign, Mul},
+};
 
 use serde::{
     de::{self, SeqAccess, Visitor},
@@ -23,7 +28,7 @@ where
     where
         A: SeqAccess<'de>,
     {
-        let mut arr = vec_to_boxed_array(T::default());
+        let mut arr = vec_to_boxed_array(vec![T::default(); S]);
 
         let mut i = 0;
         while let Some(val) = seq.next_element()? {
@@ -74,35 +79,41 @@ impl<T, const M: usize, const N: usize, const S: usize> Matrix<T, M, N, S> {
         self.inner.iter_mut()
     }
 
-    pub fn rows(&self) -> impl Iterator<Item = &[T]> {
-        self.inner.chunks_exact(N)
-    }
-
     pub fn transpose(&self) -> TransposedMatrix<T, N, M, S> {
         TransposedMatrix { inner: self }
     }
 }
 
 // TODO: may be able to replace this with somthing safe using .try_into()
-fn vec_to_boxed_array<T: Copy, const S: usize>(val: T) -> Box<[T; S]> {
-    let boxed_slice = vec![val; S].into_boxed_slice();
+fn vec_to_boxed_array<T, const S: usize>(vec: Vec<T>) -> Box<[T; S]> {
+    assert_eq!(vec.len(), S);
+    let boxed_slice = vec.into_boxed_slice();
 
     let ptr = Box::into_raw(boxed_slice) as *mut [T; S];
 
     unsafe { Box::from_raw(ptr) }
 }
 
-impl<T, const M: usize, const N: usize, const S: usize> Default for Matrix<T, M, N, S>
+impl<T, const M: usize, const N: usize, const S: usize> Matrix<T, M, N, S>
 where
-    T: Default + Copy + Serialize,
+    T: Default + Copy,
 {
-    fn default() -> Self {
-        debug_assert_eq!(M * N, S);
+    fn from_vec(vec: Vec<T>) -> Self {
         Self {
-            inner: vec_to_boxed_array(T::default()),
+            inner: vec_to_boxed_array(vec),
             _width: PhantomData,
             _height: PhantomData,
         }
+    }
+}
+
+impl<T, const M: usize, const N: usize, const S: usize> Default for Matrix<T, M, N, S>
+where
+    T: Default + Copy,
+{
+    fn default() -> Self {
+        debug_assert_eq!(M * N, S);
+        Self::from_vec(vec![T::default(); S])
     }
 }
 
@@ -114,34 +125,68 @@ pub trait MatrixMultiply<T, const M: usize, const N: usize> {
     fn multiply_by(&self, other: &[T; N]) -> [T; M];
 }
 
-impl<const M: usize, const N: usize, const S: usize> MatrixMultiply<f32, M, N>
-    for Matrix<f32, M, N, S>
+impl<T, const M: usize, const N: usize, const S: usize> MatrixMultiply<T, M, N>
+    for Matrix<T, M, N, S>
+where
+    T: Mul<Output = T> + Default + Copy + AddAssign + Sum<T>,
 {
-    fn multiply_by(&self, other: &[f32; N]) -> [f32; M] {
-        let mut result = [0.0; M];
-
-        for (i, row) in self.rows().enumerate() {
-            result[i] = row.iter().zip(other).map(|(a, b)| a * b).sum();
+    fn multiply_by(&self, other: &[T; N]) -> [T; M] {
+        let mut result = [T::default(); M];
+        for col in 0..N {
+            for row in 0..M {
+                result[row] += self.inner[row * N + col] * other[col];
+            }
         }
-
         result
     }
 }
 
-impl<'a, const M: usize, const N: usize, const S: usize> MatrixMultiply<f32, M, N>
-    for TransposedMatrix<'a, f32, M, N, S>
+impl<'a, T, const M: usize, const N: usize, const S: usize> MatrixMultiply<T, M, N>
+    for TransposedMatrix<'a, T, M, N, S>
+where
+    T: Mul<Output = T> + Default + Copy + AddAssign,
 {
-    fn multiply_by(&self, other: &[f32; N]) -> [f32; M] {
-        let mut result = [0.0; M];
-
-        for col in 0..M {
-            let mut r = 0.0;
-            for row in 0..N {
-                r += self.inner.inner[row * M + col] * other[row];
+    fn multiply_by(&self, other: &[T; N]) -> [T; M] {
+        let mut result = [T::default(); M];
+        for row in 0..N {
+            for col in 0..M {
+                result[col] += self.inner.inner[row * M + col] * other[row];
             }
-            result[col] = r;
         }
-
         result
     }
+}
+
+#[test]
+fn test_square_matrix_multiplication() {
+    let matrix: Matrix<u32, 3, 3, 9> = Matrix::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    let vec = vec_to_boxed_array(vec![97, 98, 99]);
+    let result = matrix.multiply_by(&vec);
+    assert_eq!(result, [590, 1472, 2354]);
+}
+
+#[test]
+fn test_rectangle_matrix_multiplication() {
+    let matrix: Matrix<u32, 4, 3, 12> =
+        Matrix::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    let vec = vec_to_boxed_array(vec![97, 98, 99]);
+    let result = matrix.multiply_by(&vec);
+    assert_eq!(result, [590, 1472, 2354, 3236]);
+}
+
+#[test]
+fn test_square_transposed_matrix_multiplication() {
+    let matrix: Matrix<u32, 3, 3, 9> = Matrix::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    let vec = vec_to_boxed_array(vec![97, 98, 99]);
+    let result = matrix.transpose().multiply_by(&vec);
+    assert_eq!(result, [1182, 1476, 1770]);
+}
+
+#[test]
+fn test_rectangle_transposed_matrix_multiplication() {
+    let matrix: Matrix<u32, 4, 3, 12> =
+        Matrix::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    let vec = vec_to_boxed_array(vec![96, 97, 98, 99]);
+    let result = matrix.transpose().multiply_by(&vec);
+    assert_eq!(result, [2160, 2550, 2940]);
 }
