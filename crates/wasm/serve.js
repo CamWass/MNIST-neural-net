367e086b5bc2b39e31ed6acda1e6a921dfe98695
Campbell <campbell.wass@gmail.com>
2024-01-13 12:31:43 +1300

    Make UI easier to start/serve

--- crates/wasm/serve.js
+++ crates/wasm/serve.js
@@ -2,15 +2,21 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 
+const port = 8080;
+
 http
   .createServer((req, res) => {
     let encoding = "utf8";
-    if (req.url.endsWith(".wasm")) {
+    let url = req.url;
+    if (url.endsWith(".wasm")) {
       encoding = null;
     }
-    let filePath = path.join(__dirname, "/www", req.url);
+    if (url == "/") {
+      url = "/index.html";
+    }
+    let filePath = path.join(__dirname, "/www", url);
 
-    fs.readFile(filePath, { encoding }, function (err, filedata) {
+    fs.readFile(filePath, { encoding }, function (err, fileData) {
       if (err) {
         console.error(err);
         res.writeHead(404);
@@ -18,18 +24,20 @@ http
         return;
       }
       let contentType = "text";
-      if (req.url.endsWith(".js")) {
+      if (url.endsWith(".js")) {
         contentType = "text/javascript";
-      } else if (req.url.endsWith(".html")) {
+      } else if (url.endsWith(".html")) {
         contentType = "text/html";
-      } else if (req.url.endsWith(".css")) {
+      } else if (url.endsWith(".css")) {
         contentType = "text/css";
-      } else if (req.url.endsWith(".wasm")) {
+      } else if (url.endsWith(".wasm")) {
         contentType = "application/wasm";
       }
       res.setHeader("Content-Type", contentType);
       res.writeHead(200);
-      res.end(filedata);
+      res.end(fileData);
     });
   })
-  .listen(8080, "localhost");
+  .listen(port, "localhost");
+
+console.log(`UI available at http://localhost:${port}`);
